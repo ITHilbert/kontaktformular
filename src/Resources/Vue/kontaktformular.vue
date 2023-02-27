9c096ab73b2c7a30a37fa174314b093d1e1a28cc
Jörg Hilbert <hilbert@it-entwickler-hilbert.de>
2023-02-27 10:56:45 +0100

    Edit Kontaktformular file Label

--- src/Resources/Vue/kontaktformular.vue
+++ src/Resources/Vue/kontaktformular.vue
@@ -10,7 +10,7 @@
         <input type="text" class="form-control" name="Telefon" placeholder="Telefonnummer eingeben"  required /><br>
         <label for="Nachricht" class="font-weight-bold">Ihre Nachricht *</label>
         <textarea class="form-control" name="Nachricht" placeholder="Hier Ihre Nachricht eingeben ..." required ></textarea><br>
-        <label class="font-weight-bold" for="fileUpload">Datei Anhängen (optional)<br></label>
+        <label class="font-weight-bold" for="Datei">Datei Anhängen (optional)<br></label>
         <input type="file"  name="Datei" /><br>
 
         <label class="font-weight-bold">
